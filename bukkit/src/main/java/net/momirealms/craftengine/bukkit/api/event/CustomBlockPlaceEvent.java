ca16d5a6096a32924a8c6edfdbd8a2696aa9be34
jhqwqmc <2110242767@qq.com>
2025-02-12 22:43:27 +0800

    feat(bukkit): 添加自定义方块放置事件并重构方块事件类
    - 新增 CustomBlockPlaceEvent 类用于处理自定义方块放置事件
    - 重构 CustomBlockEvent 类，使其成为自定义方块事件的基类
    - 修改 CustomBlockBreakEvent 类，使其继承自新的 CustomBlockEvent 基类
    - 在 BlockItemBehavior 中添加对 CustomBlockPlaceEvent 的处理

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/api/event/CustomBlockPlaceEvent.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/api/event/CustomBlockPlaceEvent.java
@@ -4,9 +4,8 @@ import net.momirealms.craftengine.core.block.ImmutableBlockState;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-public class CustomBlockBreakEvent extends CustomBlockEvent {
-    public CustomBlockBreakEvent(ImmutableBlockState state, Location location, Player optionalPlayer) {
+public class CustomBlockPlaceEvent extends CustomBlockEvent {
+    public CustomBlockPlaceEvent(ImmutableBlockState state, Location location, Player optionalPlayer) {
         super(state, location, optionalPlayer);
     }
 }
-
