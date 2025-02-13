3454a3de5a9ba88b73a399bbcbe04c3e4d178f7c
XiaoMoMi <littlemomi@qq.com>
2025-02-13 20:12:47 +0800

    strippable

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/behavior/BukkitBlockBehaviors.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/behavior/BukkitBlockBehaviors.java
@@ -9,11 +9,13 @@ public class BukkitBlockBehaviors extends BlockBehaviors {
     public static final Key BUSH_BLOCK = Key.from("craftengine:bush_block");
     public static final Key FALLING_BLOCK = Key.from("craftengine:falling_block");
     public static final Key LEAVES_BLOCK = Key.from("craftengine:leaves_block");
+    public static final Key STRIPPABLE_BLOCK = Key.from("craftengine:strippable_block");
 
     public static void init() {
         register(EMPTY, (block, args) -> EmptyBlockBehavior.INSTANCE);
         register(FALLING_BLOCK, FallingBlockBehavior.FACTORY);
         register(BUSH_BLOCK, BushBlockBehavior.FACTORY);
         register(LEAVES_BLOCK, LeavesBlockBehavior.FACTORY);
+        register(STRIPPABLE_BLOCK, StrippableBlockBehavior.FACTORY);
     }
 }
