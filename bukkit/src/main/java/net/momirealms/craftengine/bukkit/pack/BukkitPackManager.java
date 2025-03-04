437c0e0f0d5442b9b7725b3ea18352d529179d38
iqtester <iqtester888@gmail.com>
2025-03-03 23:40:14 -0500

    Fix Resource Pack Failed on Reload 1.20.3-

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/pack/BukkitPackManager.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/pack/BukkitPackManager.java
@@ -158,7 +158,7 @@ public class BukkitPackManager extends AbstractPackManager implements Listener {
             if (VersionHelper.isVersionNewerThan1_20_3()) {
                 info = Reflections.constructor$ServerResourcePackInfo.newInstance(uuid, url, sha1, required, ComponentUtils.adventureToMinecraft(prompt));
             } else {
-                info = Reflections.constructor$ServerResourcePackInfo.newInstance(url, sha1, required, ComponentUtils.adventureToMinecraft(prompt));
+                info = Reflections.constructor$ServerResourcePackInfo.newInstance(url + uuid, sha1, required, ComponentUtils.adventureToMinecraft(prompt));
             }
             Reflections.field$DedicatedServerProperties$serverResourcePackInfo.set(properties, Optional.of(info));
         } catch (Exception e) {
@@ -196,7 +196,7 @@ public class BukkitPackManager extends AbstractPackManager implements Listener {
 				);
 			} else {
 				packPacket = Reflections.constructor$ClientboundResourcePackPushPacket.newInstance(
-						url, sha1, ConfigManager.kickOnDeclined(), packPrompt
+						url + uuid, sha1, ConfigManager.kickOnDeclined(), packPrompt
 				);
 			}
 			if (user.decoderState() == ConnectionState.PLAY) {
