400cd53c184219a3ecbf09813169d6ea34128b68
jhqwqmc <2110242767@qq.com>
2025-03-24 07:20:06 +0800

    build(client-mod): 回退规范构建配置中的变量命名

--- client-mod/build.gradle.kts
+++ client-mod/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 
 version = property("project_version")!!
 group = property("project_group")!!
-val projectVersion: String by project
-val latestMinecraftVersion: String by project
-val loaderVersion: String by project
+val project_version: String by project
+val latest_minecraft_version: String by project
+val loader_version: String by project
 
 base {
     archivesName.set("craft-engine-fabric-mod")
@@ -57,17 +57,17 @@ dependencies {
 }
 
 tasks.processResources {
-    inputs.property("version", projectVersion)
-    inputs.property("minecraft_version", latestMinecraftVersion)
-    inputs.property("loader_version", loaderVersion)
+    inputs.property("version", project_version)
+    inputs.property("minecraft_version", latest_minecraft_version)
+    inputs.property("loader_version", loader_version)
 
     filteringCharset = "UTF-8"
 
     filesMatching("fabric.mod.json") {
         expand(
-            "version" to projectVersion,
-            "minecraft_version" to latestMinecraftVersion,
-            "loader_version" to loaderVersion
+            "version" to project_version,
+            "minecraft_version" to latest_minecraft_version,
+            "loader_version" to loader_version
         )
     }
 }
