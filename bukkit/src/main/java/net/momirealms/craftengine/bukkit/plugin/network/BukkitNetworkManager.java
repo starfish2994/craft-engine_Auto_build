0b9db0bd87635c1a1948c6edc06b7bcaf010ce87
jhqwqmc <2110242767@qq.com>
2025-04-02 16:35:19 +0800

    fix(bukkit): 删除调试信息

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/BukkitNetworkManager.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/BukkitNetworkManager.java
@@ -450,7 +450,6 @@ public class BukkitNetworkManager implements NetworkManager, Listener, PluginMes
                 NMSPacketEvent event = new NMSPacketEvent(packet);
                 onNMSPacketSend(player, event, packet);
                 if (event.isCancelled()) return;
-                System.out.println("Packet sent: " + packet);
                 super.write(context, packet, channelPromise);
                 channelPromise.addListener((p) -> {
                     for (Runnable task : event.getDelayedTasks()) {
