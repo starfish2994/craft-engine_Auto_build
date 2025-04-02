aacf82d523c8561737b5765d1f35050812f6d33c
jhqwqmc <2110242767@qq.com>
2025-04-02 15:58:48 +0800

    fix(bukkit): 阻止发送仅服务端实体包

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/PacketConsumers.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/PacketConsumers.java
@@ -658,6 +658,9 @@ public class PacketConsumers {
             if (BukkitFurnitureManager.instance().isFurnitureBaseEntity(entityId)) {
                 event.setCancelled(true);
             }
+            if (BukkitFurnitureManager.instance().isFurnitureCollisionEntity(entityId)) {
+                event.setCancelled(true);
+            }
         } catch (Exception e) {
             CraftEngine.instance().logger().warn("Failed to handle ClientboundMoveEntityPacket$Pos", e);
         }
