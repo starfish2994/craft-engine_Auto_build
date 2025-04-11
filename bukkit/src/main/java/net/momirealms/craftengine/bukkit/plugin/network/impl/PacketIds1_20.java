ba2191b6ceb01e0dea7181869d7e957430af9f15
XiaoMoMi <littlemomi@qq.com>
2025-04-12 00:27:28 +0800

    Update PacketIds1_20.java

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/impl/PacketIds1_20.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/network/impl/PacketIds1_20.java
@@ -97,6 +97,6 @@ public class PacketIds1_20 implements PacketIds {
 
     @Override
     public int clientboundPlayerInfoUpdatePacket() {
-        return PacketIdFinder.clientboundByClazz(Reflections.clazz$clientboundplayer);
+        return PacketIdFinder.clientboundByClazz(Reflections.clazz$ClientboundPlayerInfoUpdatePacket);
     }
 }
