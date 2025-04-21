25ea0aa52c2cf141ebb419ce5c9a787f1d27f44f
XiaoMoMi <littlemomi@qq.com>
2025-04-21 21:57:09 +0800

    修复wrapped物品缓存

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/TestCommand.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/TestCommand.java
@@ -1,8 +1,10 @@
 package net.momirealms.craftengine.bukkit.plugin.command.feature;
 
+import net.momirealms.craftengine.bukkit.item.BukkitItemManager;
 import net.momirealms.craftengine.bukkit.plugin.command.BukkitCommandFeature;
 import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.command.CraftEngineCommandManager;
+import net.momirealms.craftengine.core.util.Key;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.incendo.cloud.Command;
@@ -19,6 +21,7 @@ public class TestCommand extends BukkitCommandFeature<CommandSender> {
                 .senderType(Player.class)
                 .handler(context -> {
                     Player player = context.sender();
+                    player.getInventory().addItem(BukkitItemManager.instance().createWrappedItem(Key.from("default:topaz"), null).getItem());
                 });
     }
 
