f0340f3707706c75c301a2e68e498b452c2888a5
jhqwqmc <2110242767@qq.com>
2025-05-11 09:15:27 +0800

    feat(bukkit): 添加方块标签功能

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/TestCommand.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/TestCommand.java
@@ -2,6 +2,7 @@ package net.momirealms.craftengine.bukkit.plugin.command.feature;
 
 import net.momirealms.craftengine.bukkit.item.BukkitItemManager;
 import net.momirealms.craftengine.bukkit.plugin.command.BukkitCommandFeature;
+import net.momirealms.craftengine.bukkit.util.BlockTags;
 import net.momirealms.craftengine.core.item.Item;
 import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.command.CraftEngineCommandManager;
@@ -13,47 +14,51 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.Command;
+import org.incendo.cloud.bukkit.data.BlockPredicate;
+import org.incendo.cloud.bukkit.parser.BlockPredicateParser;
 import org.incendo.cloud.bukkit.parser.NamespacedKeyParser;
 import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.context.CommandInput;
+import org.incendo.cloud.parser.standard.BooleanParser;
 import org.incendo.cloud.parser.standard.ByteParser;
 import org.incendo.cloud.parser.standard.StringParser;
 import org.incendo.cloud.suggestion.Suggestion;
 import org.incendo.cloud.suggestion.SuggestionProvider;
 
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 public class TestCommand extends BukkitCommandFeature<CommandSender> {
+    public static final Collection<Suggestion> TARGET_BLOCK_SUGGESTIONS = new HashSet<>();
+
+    static {
+        for (Material material : Material.values()) {
+            TARGET_BLOCK_SUGGESTIONS.add(Suggestion.suggestion(material.getKey().toString()));
+        }
+    }
 
     public TestCommand(CraftEngineCommandManager<CommandSender> commandManager, CraftEngine plugin) {
         super(commandManager, plugin);
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public Command.Builder<? extends CommandSender> assembleCommand(org.incendo.cloud.CommandManager<CommandSender> manager, Command.Builder<CommandSender> builder) {
         return builder
                 .senderType(Player.class)
-                .required("id", NamespacedKeyParser.namespacedKeyComponent().suggestionProvider(new SuggestionProvider<>() {
+                .required("reset", BooleanParser.booleanParser())
+                .required("setTag", NamespacedKeyParser.namespacedKeyParser())
+                .required("targetBlock", StringParser.stringComponent(StringParser.StringMode.GREEDY_FLAG_YIELDING).suggestionProvider(new SuggestionProvider<>() {
                     @Override
                     public @NonNull CompletableFuture<? extends @NonNull Iterable<? extends @NonNull Suggestion>> suggestionsFuture(@NonNull CommandContext<Object> context, @NonNull CommandInput input) {
-                        return CompletableFuture.completedFuture(plugin().itemManager().cachedSuggestions());
+                        return CompletableFuture.completedFuture(TARGET_BLOCK_SUGGESTIONS);
                     }
                 }))
-                .required("displayType", ByteParser.byteParser((byte) 0, (byte) 8))
-                .required("translation", StringParser.stringParser())
-                .required("rotation", StringParser.stringParser())
                 .handler(context -> {
                     Player player = context.sender();
-                    NamespacedKey namespacedKey = context.get("id");
-                    ItemStack item = new ItemStack(Material.TRIDENT);
-                    item.editMeta((meta) -> {
-                        Item<ItemStack> ceItem = BukkitItemManager.instance().createWrappedItem(Key.of(namespacedKey.asString()), null);
-                        Optional<Integer> customModelData = ceItem.customModelData();
-                        customModelData.ifPresent(meta::setCustomModelData);
-                    });
-                    player.getInventory().addItem(item);
+                    player.sendMessage("开始测试");
+                    NamespacedKey key = context.get("setTag");
+                    BlockTags.test(plugin().adapt(player), context.get("reset"), context.get("targetBlock"), key.asString());
+                    player.sendMessage("结束测试");
                 });
     }
 
