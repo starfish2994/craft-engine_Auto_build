a1fc52f72a9a98e333e53a9d76c0bc17295a2eb7
XiaoMoMi <littlemomi@qq.com>
2025-06-02 19:50:10 +0800

    添加限制

--- bukkit/loader/src/main/java/net/momirealms/craftengine/bukkit/plugin/BukkitCraftEnginePlugin.java
+++ bukkit/loader/src/main/java/net/momirealms/craftengine/bukkit/plugin/BukkitCraftEnginePlugin.java
@@ -1,6 +1,11 @@
 package net.momirealms.craftengine.bukkit.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 public class BukkitCraftEnginePlugin extends JavaPlugin {
     private final BukkitCraftEngine plugin;
@@ -18,7 +23,22 @@ public class BukkitCraftEnginePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.plugin.scheduler().asyncRepeating(() -> {
+            Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+            if (players.size() > 20) {
+                for (Player player : players) {
+                    player.sendMessage("Better Together! This server supports 20 players (Community Edition). Want more slots & features? Ask the admin about: » Going CraftEngine Premium Edition!");
+                }
+                this.plugin.logger().warn("Glad to see that your server is growing!");
+                this.plugin.logger().warn("The Community Edition supports up to 20 players. Unlock limitless potential with CraftEngine Premium:");
+                this.plugin.logger().warn("► Unlimited player capacity");
+                this.plugin.logger().warn("► Priority support");
+                this.plugin.logger().warn("► Advanced management tools");
+            }
+        }, 1, 1, TimeUnit.MINUTES);
         this.plugin.onPluginEnable();
+        this.plugin.logger().warn("You're using the CraftEngine Community Edition");
+        this.plugin.logger().warn(" - Maximum player limit is restricted to 20");
     }
 
     @Override
--- bukkit/paper-loader/src/main/java/net/momirealms/craftengine/bukkit/plugin/PaperCraftEnginePlugin.java
+++ bukkit/paper-loader/src/main/java/net/momirealms/craftengine/bukkit/plugin/PaperCraftEnginePlugin.java
@@ -1,6 +1,11 @@
 package net.momirealms.craftengine.bukkit.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 public class PaperCraftEnginePlugin extends JavaPlugin {
     private final PaperCraftEngineBootstrap bootstrap;
@@ -18,6 +23,22 @@ public class PaperCraftEnginePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         this.bootstrap.plugin.onPluginEnable();
+        this.bootstrap.plugin.scheduler().asyncRepeating(() -> {
+            Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+            if (players.size() > 20) {
+                for (Player player : players) {
+                    player.sendMessage("Better Together! This server supports 20 players (Community Edition). Want more slots & features? Ask the admin about: » Going CraftEngine Premium Edition!");
+                }
+                this.bootstrap.plugin.logger().warn("Glad to see that your server is growing!");
+                this.bootstrap.plugin.logger().warn("The Community Edition supports up to 20 players. Unlock limitless potential with CraftEngine Premium:");
+                this.bootstrap.plugin.logger().warn("► Unlimited player capacity");
+                this.bootstrap.plugin.logger().warn("► Priority support");
+                this.bootstrap.plugin.logger().warn("► Advanced management tools");
+            }
+        }, 1, 1, TimeUnit.MINUTES);
+        this.bootstrap.plugin.onPluginEnable();
+        this.bootstrap.plugin.logger().warn("You're using the CraftEngine Community Edition");
+        this.bootstrap.plugin.logger().warn(" - Maximum player limit is restricted to 20");
     }
 
     @Override
