2cd8088109ff653822f163ea8e9d6ed7d35f31b1
jhqwqmc <2110242767@qq.com>
2025-06-06 18:37:35 +0800

    perf(entity): 减少一次冗余的调用

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/entity/data/EntityData.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/entity/data/EntityData.java
@@ -11,17 +11,17 @@ public interface EntityData<T> {
 
     default Object createEntityDataIfNotDefaultValue(T value) {
         if (defaultValue().equals(value)) return null;
-        return EntityDataValue.create(id(), serializer(), value);
+        return EntityDataValue.create(id(), serializer(), entityDataAccessor(), value);
     }
 
     default void addEntityDataIfNotDefaultValue(T value, List<Object> list) {
         if (!defaultValue().equals(value)) {
-            list.add(EntityDataValue.create(id(), serializer(), value));
+            list.add(EntityDataValue.create(id(), serializer(), entityDataAccessor(), value));
         }
     }
 
     default void addEntityData(T value, List<Object> list) {
-        list.add(EntityDataValue.create(id(), serializer(), value));
+        list.add(EntityDataValue.create(id(), serializer(), entityDataAccessor(), value));
     }
 
     static <T> EntityData<T> of(int id, Object serializer, T defaultValue) {
