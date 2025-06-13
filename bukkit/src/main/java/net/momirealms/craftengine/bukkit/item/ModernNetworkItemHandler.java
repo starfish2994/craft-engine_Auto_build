f2ad6f2ba196664cd7fdac675694f1424ba53df2
XiaoMoMi <littlemomi@qq.com>
2025-06-13 16:47:25 +0800

    一些改进

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/item/ModernNetworkItemHandler.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/item/ModernNetworkItemHandler.java
@@ -66,7 +66,8 @@ public final class ModernNetworkItemHandler implements NetworkItemHandler<ItemSt
             return new OtherItem(wrapped, false).process();
         } else {
             BukkitCustomItem customItem = (BukkitCustomItem) optionalCustomItem.get();
-            boolean hasDifferentMaterial = FastNMS.INSTANCE.method$ItemStack$getItem(wrapped.getLiteralObject()) != customItem.clientItem();
+            Object serverItem = FastNMS.INSTANCE.method$ItemStack$getItem(wrapped.getLiteralObject());
+            boolean hasDifferentMaterial = serverItem == customItem.item() && serverItem != customItem.clientItem();
             if (hasDifferentMaterial) {
                 wrapped = wrapped.unsafeTransmuteCopy(customItem.clientItem(), wrapped.count());
             }
