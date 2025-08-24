c5de57cc0f0140193f3bc30ba7b7a858e4554ca3
ThLion228 <egorvyblov24@gmail.com>
2025-08-24 23:29:18 +0500

    .

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/BlockEventListener.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/BlockEventListener.java
@@ -99,7 +99,7 @@ public final class BlockEventListener implements Listener {
                 Object ownerBlock = BlockStateUtils.getBlockOwner(blockState);
                 Object soundType = CoreReflections.field$BlockBehaviour$soundType.get(ownerBlock);
                 Object placeSound = CoreReflections.field$SoundType$placeSound.get(soundType);
-                player.playSound(block.getLocation(), FastNMS.INSTANCE.field$SoundEvent$location(placeSound).toString(), SoundCategory.BLOCKS, 1f, 0.8f);
+                player.playSound(block.getLocation().add(0.5D, 0.5D, 0.5D), FastNMS.INSTANCE.field$SoundEvent$location(placeSound).toString(), SoundCategory.BLOCKS, 1f, 0.8f);
             } catch (ReflectiveOperationException e) {
                 this.plugin.logger().warn("Failed to get sound type", e);
             }
